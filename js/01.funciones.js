e78b7685b062f5447bfd887c51a601be540697ea
jsayagot <isc.jsayago@gmail.com>
2022-04-25 10:49:22 -0500

    modificaciones para primer ejercicio de jquery

--- js/01.funciones.js
+++ js/01.funciones.js
@@ -1,11 +1,24 @@
+//javascrip tradicional
 
+//Funcion asiginada a una variable que escucha el evento click del elemento con id js-tradicional
 let iniciarEventos = function () {
-    let boton1 = document.querySelector('#boton1');
+    let boton1 = document.querySelector('#js-tradicional');
     boton1.addEventListener('click', e =>{
 
         alert('Se presionó el boton utilizando Javascript tradicional');
     }, false);
 }
 
+//jQuery
+$(document).ready( function() {
+    let boton = $('#jquery');
+    boton.click(presionarBoton);
+});
+
+function presionarBoton() {
+    alert("Mensaje mostrado utilizando jQuery");
+}
+
 //Evento DOMContentLoaded – DOM está listo, por lo que el controlador puede buscar nodos DOM, inicializar la interfaz.
 addEventListener('DOMContentLoaded', iniciarEventos, false);
+
