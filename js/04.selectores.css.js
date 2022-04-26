3ecf3545f053d0de44f8105bb5c677d80a777ac1
jsayagot <isc.jsayago@hotmail.com>
2022-04-25 23:45:41 -0500

    Trabajando con selectores css

--- js/04.selectores.css.js
+++ js/04.selectores.css.js
@@ -1,10 +1,22 @@
 
 //jQuery
-
 $(function () {
     
-    console.log('Seleccionando elementos por selectores de css');
+    //
+    let ocultarLista = function () {
+        let lista = $('#lista-2 li');
+        lista.hide();
+    }
+
+    let ocultarFila = function () {
+        let lista = $('#table-1 tr');
+        lista.css('color', 'red');
+    }
 
 
+    let boton = $('#boton-1');
+    boton.click(ocultarLista);
+
+    let fila = $('#table-1 tr');
+    fila.click(ocultarFila);
 });
-
