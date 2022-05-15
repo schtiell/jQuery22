777b17cd9746dccfed5b6f51895aa3ceec8be412
jsayagot <isc.jsayago@hotmail.com>
2022-05-15 13:40:50 -0500

    Se agregaron comentarios en el ejericicio 23 de apoyo para el codigo de jQuery

--- js/23.iterando_elementos.js
+++ js/23.iterando_elementos.js
@@ -24,17 +24,25 @@ $(function () {
 
     // Ejericico 2
 
-
+    //Funcion realizarBusqueda()
     let realizarBusqueda = function () {
+
+        //Almanacenando el listado en una variable links
         let links = $(this);
+
+        //Almacenando el valor del atributo href de cada link en una variable atributo
         let atributo = links.attr('href');
 
+        //Si el valor del atributo tiene una cadena .ar entonces le cambiar el color de fondo
         if(atributo.indexOf('.ar') != -1){
             links.css('background-color', '#FFCDD2');
         }
         
     }
 
+    //listado de enlaces dentro del elemento div con id lista-links
     let lista_enlaces = $('#lista-links a');
+
+    //recorrer cada uno de los elemento seleccionados y instanciando a la funcion realizarBusqueda() a ellos
     lista_enlaces.each(realizarBusqueda);
 });
