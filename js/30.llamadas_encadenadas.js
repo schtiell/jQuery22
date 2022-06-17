630928f91a8582d4c98e91b803cacf0b3851a3bb
jsayagot <isc.jsayago@gmail.com>
2022-06-17 18:35:45 -0500

    Agregada la seccion de horoscopos en el ejercicio 30 sobre llamadas encadenadas

--- js/30.llamadas_encadenadas.js
+++ js/30.llamadas_encadenadas.js
@@ -6,14 +6,27 @@ $(function () {
     //Boton 1
     let boton1 = $('#boton-1');
 
-    //Uso de funciones anónimas y encadenamiento de métodos
+    //Uso de funciones anónimas 
     boton1.click(function (e) { 
-        $('#descripcion').fadeTo('slow', 0.5).hide('slow');
+
+        //Encadenamiento de métodos. 
+        $('#descripcion').fadeTo('slow', 0.5, 'swing').hide('slow');
     });
 
     //Boton 2
     let boton2 = $('#boton-2');
     boton2.click(function (e){
-        $('#descripcion').show().fadeTo("slow",1);
+
+        //Es importante notar que los métodos se ejecutan de izquierda a derecha.
+        $('#descripcion').show().fadeTo("slow",1, "linear");
     });
+
+
+    //Ejercicio 2: Horoscopos
+
+    $('#menu a').click(function () {
+        $('#detalles').load($(this).attr('href'));
+        return false;
+    })
+    
 });
