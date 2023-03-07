b1f709cebd8f00cf1932f157001caefcb3f7bad8
jsayagot <isc.jsayago@hotmail.com>
2023-03-06 18:15:13 -0600

    jQuery: selectores de elementos html

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Una manera diferente de trabajar con javascript del lado del cliente.
 
 1. jQuery: Una manera diferente de trabajar con javascript utilizando jQuery del lado del cliente.
 2. jQuery: Selectores, por el #id de un elemento del DOM.
+3. jQuery: Selectores, por el elemento html.
--- js/03.select.elements.js
+++ js/03.select.elements.js
@@ -5,26 +5,23 @@ $(function() {
 
     let presionarFila = function(){
 
-        //Se almacena en fila la fila sobre la que se esta haciendo click
-        let fila = $(this);
-        console.info(fila);
+        let row = $(this);  //Se almacena en row la fila sobre la que se esta haciendo click
+        console.info(row);
 
-        //Y se aplica sobre este elemento el cambio de color de fondo
-        fila.css('background-color','#eeeeee');
-        fila.css('border','#000');
+        row.css('background-color','#A3E4D7');
+        row.css('border','#000');
     }
 
 
-    let ocularElemento = function () {
-        let elemento_lista = $(this);
-        console.log(elemento_lista);
-        //elemento_lista.css('display','none');
-        elemento_lista.hide();
+    let ocularElemento = () => {
+        let item = $(this);
+        console.log(item);
+        item.hide();
     }
 
     let fila = $('tr');
     fila.click(presionarFila);
 
-    let elemento_lista = $('li');
-    elemento_lista.click(ocularElemento)
+    let item = $('li');
+    item.click(ocularElemento);
 });
